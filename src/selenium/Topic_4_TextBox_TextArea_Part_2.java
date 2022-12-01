a7b0ae6ea3a58a5fdbc9e47a3d2e98bd18729a20
luanttruong <truongluan100695@gmail.com>
2022-12-01 16:16:14 +0700

    update topic_4_part_2

--- src/selenium/Topic_4_TextBox_TextArea_Part_2.java
+++ src/selenium/Topic_4_TextBox_TextArea_Part_2.java
@@ -7,29 +7,27 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.json.Json;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Topic_4_TextBox_TextArea_Part_2{
+public class Topic_4_TextBox_TextArea_Part_2 {
 	WebDriver driver;
 	Actions action;
 	String geckodriver = "/Users/geotech/Downloads/Setup-Mac/geckodriver";
-	
-	//Data test
+
+	// Data test
 	String employeeID;
 	String Username = "Admin";
 	String Password = "admin123";
-	String FirstName = "Gracy";
+	String FirstName = "Grace";
 	String LastName = "Ngo";
-	String UserNameCreateEmployee = "gracyngo";
+	String UserNameCreateEmployee = "gracengo";
 	String PasswordCreateEmployee = "Gpp3FprM.2dLXr7G";
 	String ConfirmPasswordCreateEmployee = "Gpp3FprM.2dLXr7G";
-	
-	
-	//Elements	
+
+	// Elements
 	By txtUsername = By.name("username");
 	By txtPassword = By.name("password");
 	By btnLogin = By.xpath("//button[text()=' Login ']");
@@ -44,46 +42,46 @@ public class Topic_4_TextBox_TextArea_Part_2{
 	By checkboxCreateLoginDetails = By.xpath("//input[@type='checkbox']/following-sibling::span");
 	By txtUserNameCreateEmployee = By.xpath("//label[text()='Username']/parent::div/following-sibling::div/input");
 	By txtPasswordCreateEmployee = By.xpath("//label[text()='Password']/parent::div/following-sibling::div/input");
-	By txtConfirmPasswordCreateEmployee = By.xpath("//label[text()='Confirm Password']/parent::div/following-sibling::div/input");
+	By txtConfirmPasswordCreateEmployee = By
+			.xpath("//label[text()='Confirm Password']/parent::div/following-sibling::div/input");
 	By btnSave = By.xpath("//button[text()=' Save ']");
 	By hdPersonalDetails = By.xpath("//h6[text()='Personal Details']");
 	By btnLogout = By.linkText("Logout");
 	By hdLogin = By.xpath("//h5[text()='Login']");
 	By btnMyInfo = By.linkText("My Info");
-			
+
 	boolean CheckboxIsChecked(By by) {
 		WebElement element = driver.findElement(by);
-		if(element.isSelected()) {
+		if (element.isSelected()) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
-	
+
 	@BeforeClass
 	public void beforeClass() {
-		System.setProperty("webdriver.gecko.driver", geckodriver );
+		System.setProperty("webdriver.gecko.driver", geckodriver);
 		driver = new FirefoxDriver();
 		action = new Actions(driver);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-		
-		//Login
+
+		// Login
 		driver.findElement(txtUsername).sendKeys(Username);
 		driver.findElement(txtPassword).sendKeys(Password);
 		driver.findElement(btnLogin).click();
-		
-		//Verify dashboard page is displayed
+
+		// Verify dashboard page is displayed
 		Assert.assertTrue(driver.findElement(dashboard).isDisplayed());
-		
+
 	}
 
 	@Test
 	public void TC_01_AddEmployee() throws InterruptedException {
 		driver.findElement(btnPim).click();
-		//Verify Pim page is displayed
+		// Verify Pim page is displayed
 		Assert.assertTrue(driver.findElement(pimHeader).isDisplayed());
 		driver.findElement(btnAddEmployee).click();
 		Assert.assertTrue(driver.findElement(addEmployeeHeader).isDisplayed());
@@ -92,41 +90,41 @@ public class Topic_4_TextBox_TextArea_Part_2{
 		employeeID = driver.findElement(txtEmployeeID).getAttribute("value");
 		System.out.println("employeeID: " + employeeID);
 		Thread.sleep(10000);
-		
+
 		driver.findElement(checkboxCreateLoginDetails).click();
 		driver.findElement(txtUserNameCreateEmployee).sendKeys(UserNameCreateEmployee);
 		driver.findElement(txtPasswordCreateEmployee).sendKeys(PasswordCreateEmployee);
 		driver.findElement(txtConfirmPasswordCreateEmployee).sendKeys(ConfirmPasswordCreateEmployee);
 		driver.findElement(btnSave).click();
 		Thread.sleep(10000);
-		
+
 		Assert.assertTrue(driver.findElement(hdPersonalDetails).isDisplayed());
 		Assert.assertEquals(driver.findElement(txtFirstName).getAttribute("value"), FirstName);
 		Assert.assertEquals(driver.findElement(txtLastName).getAttribute("value"), LastName);
-		Assert.assertEquals(driver.findElement(txtEmployeeID).getAttribute("value"),employeeID);
-		
-		//Logout
+		Assert.assertEquals(driver.findElement(txtEmployeeID).getAttribute("value"), employeeID);
+
+		// Logout
 		driver.findElement(By.xpath("//img/following-sibling::p")).click();
 		driver.findElement(btnLogout).click();
 		Thread.sleep(10000);
-		
-		//Login
+
+		// Login
 		Assert.assertTrue(driver.findElement(hdLogin).isDisplayed());
 		driver.findElement(txtUsername).sendKeys(UserNameCreateEmployee);
 		driver.findElement(txtPassword).sendKeys(PasswordCreateEmployee);
 		driver.findElement(btnLogin).click();
 		Thread.sleep(10000);
 		Assert.assertTrue(driver.findElement(dashboard).isDisplayed());
-		
-		//Verify my info after login successfully
+
+		// Verify my info after login successfully
 		driver.findElement(btnMyInfo).click();
 		Thread.sleep(10000);
 		Assert.assertTrue(driver.findElement(hdPersonalDetails).isDisplayed());
 		Assert.assertEquals(driver.findElement(txtFirstName).getAttribute("value"), FirstName);
 		Assert.assertEquals(driver.findElement(txtLastName).getAttribute("value"), LastName);
-		Assert.assertEquals(driver.findElement(txtEmployeeID).getAttribute("value"),employeeID);
+		Assert.assertEquals(driver.findElement(txtEmployeeID).getAttribute("value"), employeeID);
 	}
-	
+
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
